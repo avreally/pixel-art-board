d1e033e2470aaa89edc2b042c88e9719bc3cc7bd
Valeria <waleria.shadrina@gmail.com>
2025-10-19 19:45:44 +0200

    Add ClearCanvas component with styling

---
+++ src/components/ClearCanvas/ClearCanvas.tsx
@@ -0,0 +1,14 @@
+"use client";
+import styles from "./ClearCanvas.module.css";
+
+type ClearCanvasProps = {
+  handleClearCanvasClick: () => void;
+};
+
+export const ClearCanvas = ({ handleClearCanvasClick }: ClearCanvasProps) => {
+  return (
+    <button className={styles.button} onClick={handleClearCanvasClick}>
+      Clear canvas
+    </button>
+  );
+};
