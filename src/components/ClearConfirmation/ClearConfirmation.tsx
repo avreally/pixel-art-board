fba328a0f0b95b2dfe72778a1ab7c46e25fbddaf
Valeria <waleria.shadrina@gmail.com>
2025-10-19 19:45:29 +0200

    Add ClearConfirmation and Modal components with styles

---
+++ src/components/ClearConfirmation/ClearConfirmation.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import styles from "./ClearConfirmation.module.css";
+
+type ClearConfirmationProps = {
+  onConfirm?: () => void;
+  onCancel: () => void;
+};
+
+export const ClearConfirmation = ({
+  onConfirm,
+  onCancel,
+}: ClearConfirmationProps) => {
+  return (
+    <>
+      <h3 className={styles.title}>Clear canvas?</h3>
+      <p className={styles.text}>
+        Are you sure you want to clear current canvas?
+      </p>
+
+      <div className={styles.buttons}>
+        <button className={clsx(styles.button)} onClick={onCancel}>
+          Cancel
+        </button>
+        <button
+          className={clsx(styles.button, styles.clear)}
+          onClick={onConfirm}
+        >
+          Clear
+        </button>
+      </div>
+    </>
+  );
+};
